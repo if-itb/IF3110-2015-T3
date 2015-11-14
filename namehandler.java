6f75c9ab754d9041cbf45e77939a440cd52e2e90
Steven Andianto <stevenandianto@gmail.com>
2015-11-14 23:02:46 +0700

    Testing Membuat Form dan Menuliskan Form di JSP

---
+++ namehandler.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.hello;
+
+/**
+ *
+ * @author user
+ */
+public class namehandler {
+    private String nama;
+    public namehandler() {nama = null;};
+
+    /**
+     * @return the name
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the name to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    
+}
