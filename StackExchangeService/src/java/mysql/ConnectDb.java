3816888c87ddc40cd0d8f743ab0000093d9f748b
Aufar Gilbran <aufargilbran@gmail.com>
2015-11-15 15:37:40 +0700

    Create authentication service

--- StackExchangeService/src/java/mysql/ConnectDb.java
+++ StackExchangeService/src/java/mysql/ConnectDb.java
@@ -4,19 +4,22 @@ import java.sql.*;
 
 public class ConnectDb {
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost:3306/stackExchange?zeroDateTimeBehavior=convertToNull";
+   static final String DB_URL = "jdbc:mysql://localhost:3306/re-asklyz?zeroDateTimeBehavior=convertToNull";
 
-   static final String USER = "root";
+   static final String USER = "hebakusa";
    static final String PASS = "";
    
    static Connection conn = null;
    public static Connection connect() {
-       try{
-           Class.forName("com.mysql.jdbc.Driver");
-           System.out.println("Connecting to database...");
-           conn = (Connection) DriverManager.getConnection(DB_URL,USER,PASS);
-        }catch(SQLException | ClassNotFoundException se){
-        }finally{
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Connecting to database...");
+            conn = (Connection) DriverManager.getConnection(DB_URL,USER,PASS);
+        }
+        catch(SQLException | ClassNotFoundException se){
+            System.out.println("Failed to connect to the database.");
+        }
+        finally {
             return conn;
         }
    }
