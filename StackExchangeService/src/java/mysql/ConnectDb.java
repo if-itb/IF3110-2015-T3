289744120b3d9c90f9e259cd57e49dac635d46ae
gbusiri <13513041@std.stei.itb.ac.id>
2015-11-15 14:13:03 +0700

    create frontend log in

--- StackExchangeService/src/java/mysql/ConnectDb.java
+++ StackExchangeService/src/java/mysql/ConnectDb.java
@@ -7,7 +7,7 @@ public class ConnectDb {
    static final String DB_URL = "jdbc:mysql://localhost:3306/stackExchange?zeroDateTimeBehavior=convertToNull";
 
    static final String USER = "root";
-   static final String PASS = "root";
+   static final String PASS = "";
    
    static Connection conn = null;
    public static Connection connect() {
