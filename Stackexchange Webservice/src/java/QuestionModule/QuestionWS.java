7a54e6b5a559482f97a100e475a388bf19f3bea4
deredet <lucky.cahyadi@yahoo.com>
2015-11-16 12:39:07 +0700

    fix QuestionWS.java

--- Stackexchange Webservice/src/java/QuestionModule/QuestionWS.java
+++ Stackexchange Webservice/src/java/QuestionModule/QuestionWS.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
 
 import DatabaseModule.Database;
 import QuestionModule.Question;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -29,22 +31,36 @@ public class QuestionWS {
     Connection con = DB.connect();
     
     @WebMethod(operationName = "GetAllQuestion")
-    @WebResult(name="Questions");
+    @WebResult(name="Questions")
     public ArrayList<Question> GetAllQuestion() {
-       ArrayList<Question> Questions = new ArrayList<Question>;
+       ArrayList<Question> Questions = new ArrayList<>();
        try{
            String query = "SELECT * FROM QUESTIONS";
            PreparedStatement st = con.prepareStatement(query);
            ResultSet rs = st.executeQuery();
            while(rs.next()){
-               Questions.add(new Question(rs.getInt()))
-           
+               Questions.add(new Question(rs.getInt("qid"), rs.getInt("uid"), rs.getString("Content"),rs.getString("TIMESTAMP") ));
            }
-       }
-       catch{
-       
-       }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Questions;
     }
 
-    
+    @WebMethod(operationName = "GetQuestionByID")
+    @WebResult(name="Question")
+    public Question GetQuestionByID(int qid){
+        Question q = null;
+        try {
+            String query = "SELECT * FROM QUESTIONS WHERE ?";
+            PreparedStatement st = con.prepareStatement(query);
+            st.setInt(1,qid);
+            ResultSet rs = st.executeQuery();
+            q = new Question(qid,rs.getInt("uid"),rs.getString("Content"),rs.getString("TIMESTAMP"));
+        } catch (SQLException ex) {
+            Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return q;
+    }
 }
