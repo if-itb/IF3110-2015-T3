d62d5a7aedf13adbf766260bef940f00e8592c37
deredet <lucky.cahyadi@yahoo.com>
2015-11-17 07:14:02 +0700

    fix QuestionWS

--- Stackexchange Webservice/src/java/QuestionModule/QuestionWS.java
+++ Stackexchange Webservice/src/java/QuestionModule/QuestionWS.java
@@ -26,41 +26,162 @@ import java.util.logging.Logger;
  */
 @WebService(serviceName = "QuestionWS")
 public class QuestionWS {
-    
-    Database DB = new Database();
-    Connection con = DB.connect();
-    
     @WebMethod(operationName = "GetAllQuestion")
     @WebResult(name="Questions")
     public ArrayList<Question> GetAllQuestion() {
-       ArrayList<Question> Questions = new ArrayList<>();
-       try{
-           String query = "SELECT * FROM QUESTIONS";
-           PreparedStatement st = con.prepareStatement(query);
-           ResultSet rs = st.executeQuery();
-           while(rs.next()){
-               Questions.add(new Question(rs.getInt("qid"), rs.getInt("uid"), rs.getString("Content"),rs.getString("TIMESTAMP") ));
-           }
+        Database DB = new Database();
+        Connection con = DB.connect();
 
+        ArrayList<Question> Questions = new ArrayList<>();
+        Statement stmt = null;
+        ResultSet rs = null;
+        try{
+            stmt = con.createStatement();
+            String query = "SELECT * FROM QUESTIONS";
+            rs = stmt.executeQuery(query);
+            while(rs.next()){
+                Questions.add(new Question(rs.getInt("qid"), rs.getString("Email"),rs.getString("QuestionTopic"), rs.getString("Content"),rs.getInt("vote"), rs.getString("TIMESTAMP") ));
+            }
+            rs.close();
         } catch (SQLException ex) {
-            Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+           ex.printStackTrace();
+        } catch (Exception ex){
+           ex.printStackTrace();
+        } finally{
+            try{
+                if(stmt!=null)    con.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+            try{
+                if(con!=null)    con.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
         }
-        return Questions;
+         return Questions;
     }
 
     @WebMethod(operationName = "GetQuestionByID")
     @WebResult(name="Question")
-    public Question GetQuestionByID(int qid){
+    public Question GetQuestionByID(@WebParam(name="qid")int qid){
+        Database DB = new Database();
+        Connection con = DB.connect();
+
         Question q = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
         try {
             String query = "SELECT * FROM QUESTIONS WHERE ?";
-            PreparedStatement st = con.prepareStatement(query);
-            st.setInt(1,qid);
-            ResultSet rs = st.executeQuery();
-            q = new Question(qid,rs.getInt("uid"),rs.getString("Content"),rs.getString("TIMESTAMP"));
+            ps = con.prepareStatement(query);
+            ps.setInt(1,qid);
+            rs = ps.executeQuery();
+            q = new Question(rs.getInt("qid"), rs.getString("Email"),rs.getString("QuestionTopic"), rs.getString("Content"),rs.getInt("vote"), rs.getString("TIMESTAMP") );
+            rs.close();
         } catch (SQLException ex) {
-            Logger.getLogger(QuestionWS.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            try{
+                if(ps!=null)    con.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+            try{
+                if(con!=null)    con.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
         }
         return q;
     }
+    
+    @WebMethod(operationName = "InsertQuestion")
+    public void InsertQuestion(@WebParam(name = "Q") Question Q){
+        Database DB = new Database();
+        Connection con = DB.connect();
+        PreparedStatement ps=null;
+        try{
+            String query = "INSERT INTO QUESTIONS VALUES (?,?,?,?)";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, Q.getQid());
+            ps.setString(2, Q.getUemail());
+            ps.setString(3,Q.getQtopic());
+            ps.setString(4,Q.getQcontent());
+            ps.executeUpdate();
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            try{
+                if(ps!=null)    con.close();
+            }catch(SQLException ex ){
+                ex.printStackTrace();
+            }
+            try{
+                if(con!=null)    con.close();
+            }catch(SQLException ex ){
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    @WebMethod(operationName="UpdateQuestion")
+    public void UpdateQuestion(@WebParam(name="Q") Question Q){
+        Database DB = new Database();
+        Connection con = DB.connect();
+        PreparedStatement ps=null;
+        try{
+            String query = "UPDATE QUESTIONS SET QuestionTopic = ?, Content = ? where qid = ?";
+            ps = con.prepareStatement(query);
+            ps.setString(1,Q.getQtopic());
+            ps.setString(2, Q.getQcontent());
+            ps.setInt(3,Q.getQid());
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            try{
+                if(ps!=null)    con.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+            try{
+                if(ps!=null)    con.close();     
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    @WebMethod(operationName="DeleteQuestion")
+    public void DeleteQuestion(@WebParam(name="Q")Question Q){
+        Database DB = new Database();
+        Connection con = DB.connect();
+        PreparedStatement ps=null;
+        try{
+            String query = "DELETE FROM QUESTION WHERE qid = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, Q.getQid());
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            try{
+                if(ps!=null) con.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+            try{
+                if(con!=null) con.close();
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+    
 }
