debb4a8501fd81efe9e019a1ce4e835585a6c1fe
levanjipm <myc.glace@gmail.com>
2015-11-18 17:31:14 +0700

    insert Question Web Service

--- StackExchange WS/src/java/QuestionModel/QuestionWS.java
+++ StackExchange WS/src/java/QuestionModel/QuestionWS.java
@@ -6,6 +6,10 @@
 package QuestionModel;
 
 import DatabaseWS.DB;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -32,6 +36,7 @@ public class QuestionWS {
     
     /**
      * Web service operation
+     * @return 
      */
     @WebMethod(operationName = "getQuestion")
     @WebResult(name="Question")
@@ -65,5 +70,70 @@ public class QuestionWS {
         }
         
         return questions;
+    }
+
+    /**
+     * Web service operation
+     * @param token
+     * @param content
+     * @param title
+     */
+    @WebMethod(operationName = "insertQuestion")
+    @WebResult(name = "insQuestion")
+    public int insertQuestion(@WebParam(name = "access_token") String token,
+                                    @WebParam(name = "title") String title,
+                                    @WebParam(name = "content") String content)
+    throws Exception {
+        // cek token (kasih IS)
+        
+        int Valid;
+       /* String url = "localhost:8082/?access_token=" + token;
+        URL obj = new URL(url);
+        
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        String USER_AGENT = "Mozilla/5.0";
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'Get' request to " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        
+        while ((inputLine = in.readLine()) != null){
+            response.append(inputLine);
+            
+        }
+        in.close();
+                
+        System.out.println(response.toString());
+        
+        if (response.toString() == token){*/
+            try {
+                Statement stmt = conn.createStatement();
+                String sql;
+                sql = "INSERT INTO question (q_topic, q_content) VALUES (?, ?)";
+
+                PreparedStatement dbStatement = conn.prepareStatement(sql);
+                dbStatement.setString(1, title);
+                dbStatement.setString(2, content);
+
+                ResultSet rs = dbStatement.executeQuery();
+                rs.close();
+                stmt.close();
+                Valid = 1;
+            } catch (SQLException e){
+                Valid = 0;
+            }
+            
+       /* }
+        else {
+            Valid = 0;
+        }*/
+        return Valid;
+       
     }
 }
