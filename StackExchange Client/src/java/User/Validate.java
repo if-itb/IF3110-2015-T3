f28d841413276b3b4ad62bf9a3679e71668c418d
bayuRP <13513046@std.stei.itb.ac.id>
2015-12-05 11:01:24 +0700

    user validdation added

---
+++ StackExchange Client/src/java/User/Validate.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package User;
+
+import Tools.Tools;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author User
+ */
+public class Validate extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      System.setProperty("java.net.preferIPv6Addresses", "false");
+        
+      Tools tools = new Tools();
+      String access_token = tools.getCookie("access_token", request);
+        
+      String userIP = request.getHeader("X-FORWARDED-FOR");  
+      if (userIP == null) {  
+          userIP = request.getRemoteAddr();  
+      }
+      String userAgent = request.getHeader("User-Agent");
+      request.setAttribute("userIP", userIP);
+      request.setAttribute("userAgent", userAgent);
+      userAgent = userIP.toLowerCase() + userAgent.toLowerCase();
+      
+      boolean valid = false;
+      if (!access_token.equals(null) && access_token.length() > userAgent.length())
+          valid = access_token.substring(userAgent.length()).toLowerCase().equals(userAgent);
+      
+      System.out.println(access_token.toLowerCase()+ " " + userAgent + " " + valid);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
