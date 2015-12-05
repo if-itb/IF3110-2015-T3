933ea152b256a526cdbe457324d0024d2057b4ae
Tifani Warnita <tiffayumuyuka@gmail.com>
2015-12-06 03:24:34 +0700

    Add connector and comment servlet

---
+++ StackExchange VoteCommentService/src/java/models/Comment.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Tifani
+ */
+public class Comment {
+    private int cId;
+    private int qId;
+    private int uId;
+    private String content;
+    private String dateCreated;
+    
+    public Comment() {
+        
+    }
+
+    public Comment(int cId, int qId, int uId, String content, String dateCreated) {
+        this.cId = cId;
+        this.qId = qId;
+        this.uId = uId;
+        this.content = content;
+        this.dateCreated = dateCreated;
+    }
+}
