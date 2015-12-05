764c9dc04da5e802d316492c968010d7a54ac16b
Natan Elia <natanelia7@gmail.com>
2015-12-05 17:34:49 +0700

    Enable CORS

--- VoteComment/app.js
+++ VoteComment/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var answerComment = require('./routes/answercomment'); //routes are defined here
 var questionComment = require('./routes/questioncomment');
 var answer = require('./routes/answer');
@@ -10,6 +11,7 @@ var app = express(); //Create the Express app
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 app.use('/', answerComment); //This is our route middleware
 app.use('/', questionComment); //This is our route middleware
 app.use('/', answer); //This is our route middleware
