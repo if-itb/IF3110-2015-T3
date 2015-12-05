1d8232411a87b2f5cd1c79ef165c2f5774872eea
Venny Larasati <venny_la@yahoo.com>
2015-12-05 16:42:58 +0700

    Add project for Comment-Vote service

---
+++ Comment-Vote_Service/src/java/vote/Vote.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vote;
+
+import connection.DB;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Venny
+ */
+@WebServlet(name = "Vote", urlPatterns = {"/vote"})
+public class Vote extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Connection conn = DB.connect();
+        response.setContentType("application/json");
+        
+        int id = Integer.parseInt(request.getParameter("id")); //question_id atau answer_id
+        int vote = Integer.parseInt(request.getParameter("vote"));
+        String db = request.getParameter("db");
+        String token = request.getParameter("token");
+        
+        // cek token ke IS dapetin user ID
+        
+        if (db.equals("question")){
+            //vote question
+        } else if (db.equals("answer")){
+            //vote answer
+        }
+            
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    
+    private void voteQuestion(int question_id, int vote){
+        
+    }
+    
+    private void voteAnswer(int answer_id, int vote){
+        
+    }
+}
