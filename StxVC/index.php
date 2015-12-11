13d10d2c90dbdddf8d736c681ae548c3698ceeb8
ahmadaidin <ahmadaidin08.aa@gmail.com>
2015-12-11 15:00:50 +0700

    bobok

--- StxVC/index.php
+++ StxVC/index.php
@@ -3,7 +3,7 @@
 	session_start();
 	/*	format untuk post question http://localhost/StxVC?id=
 		id yang dimaksud adalah id question
-		
+
 		format vote http://localhost/StxVC?cat=..&id=..&val=..
 		cat = kategori, a untuk answer, q untuk question
 		id = id answer/question
@@ -17,9 +17,9 @@
 			if(isset($_POST['content']) && isset($_GET['id'])) {
 			$q_id = $_GET['id'];
 			$content = $_POST['content'];
-			postAnswer($q_id, $token,$uag, $ip,$content);
+			postComment($q_id, $token,$uag, $ip,$content);
 		}
-		
+
 		//vote
 		if(isset($_GET['id']) && isset($_GET['cat']) && isset($_GET['val'])) {
 			$id = $_GET['id'];
@@ -34,5 +34,5 @@
 	} else {
 		header("HTTP/1.1 404 tidak ada token");
 	}
-	
+
 ?>
